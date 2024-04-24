7776aac2efe775fd43ca29f6406eb13a8a9001af
Java-Sith <prieto.luisdaniel@gmail.com>
2024-04-23 23:04:34 -0600

    Tiling attempt
    Must try to integrate further in ggml

--- cpp_kernels/hw-matmul-elementwise/HW/config.h
+++ cpp_kernels/hw-matmul-elementwise/HW/config.h
@@ -11,6 +11,7 @@ static constexpr int kDataWidth = 16;
 static constexpr int kDataInt = 6;
 static constexpr int kPackets = kBusWidth / kDataWidth;
 static constexpr int kShiftData = 2; // Packets 4
+static constexpr int TILE_SIZE = 16;
 
 using RawDataT = ap_uint<kBusWidth>;
 using StreamT = hls::stream<RawDataT>;
